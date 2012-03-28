642a931b2bd0783afea71eff464cdb10974127f1
Muccy <muccymac@gmail.com>
2012-03-28 14:13:38 +0200

    First Commit

---
+++ MUKToolkit/MUKToolkit.h
@@ -0,0 +1,1 @@
+#import <MUKToolkit/MUK.h>
---
+++ MUKToolkit/Unit Tests/MUKToolkitTests.h
@@ -0,0 +1,13 @@
+//
+//  MUKToolkitTests.h
+//  MUKToolkitTests
+//
+//  Created by Marco Muccinelli on 28/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MUKToolkitTests : SenTestCase
+
+@end
