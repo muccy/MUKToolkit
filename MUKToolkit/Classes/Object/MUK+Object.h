7ee1e784f14b239a8f87d0af01ed005a8e317f58
Muccy <muccymac@gmail.com>
2012-03-30 22:37:13 +0200

    Object methods and appledoc

---
+++ MUKToolkit/Classes/Object/MUK+Object.h
@@ -0,0 +1,57 @@
+// Copyright (c) 2012, Marco Muccinelli
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+// * Neither the name of the <organization> nor the
+// names of its contributors may be used to endorse or promote products
+// derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#import "MUK.h"
+
+@interface MUK (Object)
+
+/**
+ Instantiate an object from a NIB file.
+ 
+ This method could be very useful in order to load views. Say you developed a
+ `UIView` subclass called `MyView`: you can design your view into Interface Builder
+ (in `MyView.xib`) and, then, you load view using:
+    MyView *myView = [MUK objectOfClass:[MyView class] instantiatedFromNibNamed:nil
+                                 bundle:nil owner:nil options:nil atIndex:0];
+ 
+ @param objectClassOrNil Class object to instantiate. If you specify it, then `nibNameOrNil`
+ could be calculated at runtime and returned object is type checked. You could also
+ leave this parameter `nil` but, in that case, you have to specify `nibNameOrNil`.
+ @param nibNameOrNil Name of NIB file containing object to be instantiated. You could
+ leave this parameter `nil` and it will default to the name of `objectClassOrNil`.
+ @param bundleOrNil Bundle containing NIB file. If `nil` it defaults to `[NSBundle mainBundle]`.
+ @param owner Owner object which will be used during instantiation. It could be 
+ `nil` if you do not need it.
+ @param options Options dictionary used during instantiation. Not required; see
+ `UINib` for details.
+ @param index Index of object into NIB file.
+ @return Instantiated object. It could return `nil` if bundle could not be found,
+ if object could not be instantiated, if object `index` is invalid or if type
+ checking does not succeed.
+ */
+// Memo: I can not write Unit Tests because of runtime failures during instantiation
++ (id)objectOfClass:(Class)objectClassOrNil instantiatedFromNibNamed:(NSString *)nibNameOrNil bundle:(NSBundle *)bundleOrNil owner:(id)owner options:(NSDictionary *)options atIndex:(NSInteger)index;
+
+@end
--- MUKToolkit/MUKToolkit.h
+++ MUKToolkit/MUKToolkit.h
@@ -2,5 +2,6 @@
 #import <MUKToolkit/MUK+Array.h>
 #import <MUKToolkit/MUK+Date.h>
 #import <MUKToolkit/MUK+Geometry.h>
+#import <MUKToolkit/MUK+Object.h>
 #import <MUKToolkit/MUK+String.h>
 #import <MUKToolkit/MUK+URL.h>
